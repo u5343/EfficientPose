7f21b9d9823fc349ae789f334385d27914e648eb
u5343 <abanli12138@outlook.com>
2026-02-26 12:11:20 +0800

    ignoreitplz

--- calculate_offset.py
+++
@@ -1,5 +0,0 @@
-from tools.calculate_offset import main
-
-
-if __name__ == "__main__":
-    main()
--- testdataset.py
+++
@@ -1,5 +0,0 @@
-from scripts.test_dataset import main
-
-
-if __name__ == "__main__":
-    main()
